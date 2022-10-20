af0d2d7fbed26e59727cfedf2c33a33448f11bc7
Andre Miranda <andy.lm0123@gmail.com>
2022-10-20 20:48:29 -0300

    exercicios 4.1 parte 2

--- fundamentos/s04-introducao-a-javascript/dia-01/exercicios/script.js
+++ fundamentos/s04-introducao-a-javascript/dia-01/exercicios/script.js
@@ -12,3 +12,15 @@ console.log(subtracao);
 console.log(multiplicacao);
 console.log(divisao);
 console.log(modulo);
+
+
+//comparacao entre A e B.
+let compara;
+if (a > b){
+    compara = "A é maior que B";
+} else if (a < b){
+    compara = "B é maior que A";
+}else{
+    compara = "Ambos possuem o mesmo valor.";
+}
+console.log(compara);
