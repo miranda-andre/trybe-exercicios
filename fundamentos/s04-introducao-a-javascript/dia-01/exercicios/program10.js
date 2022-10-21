68ca0f8f2fb35c46a749385dd6d0ed0a13a4c54e
Andre Miranda <andy.lm0123@gmail.com>
2022-10-20 22:07:14 -0300

    exercicios 4.1 parte 10

---
+++ fundamentos/s04-introducao-a-javascript/dia-01/exercicios/program10.js
@@ -0,0 +1,10 @@
+const productCost = 1500;
+let productTotalcost;
+const productSell = 2100;
+const tax = 1.2
+
+productTotalcost = productCost * tax;
+let profit = productSell - productTotalcost;
+let total = profit * 100
+
+console.log(total)
