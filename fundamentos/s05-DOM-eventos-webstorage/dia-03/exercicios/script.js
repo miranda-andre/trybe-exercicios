fb2f707c03a1ba8d05345e5d36fc32c8e3e43a3f
Andre Miranda <andy.lm0123@gmail.com>
2022-11-01 20:33:53 -0300

    Parte 1 Calendario feito

--- fundamentos/s05-DOM-eventos-webstorage/dia-03/exercicios/script.js
+++ fundamentos/s05-DOM-eventos-webstorage/dia-03/exercicios/script.js
@@ -14,14 +14,30 @@ const createDaysOfTheWeek = () => {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 1, 2];
+
   const createDays = () => {
-    const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
     for (let i = 0; i < decemberDaysList.length; i++){
         const monthDays = document.getElementById("days");
         let days = document.createElement("li");
         days.innerHTML = decemberDaysList[i];
+        days.classList.add("day")
+        
+
+        if(decemberDaysList[i] == "24" || decemberDaysList[i] == "25" || decemberDaysList[i] == "31"){
+          days.classList.add("holiday")
+        }
         monthDays.appendChild(days);
     }
   }
   createDays();
+
+  const fridayDay = () => {
+    const friday = document.querySelectorAll(".day:nth-child(7n+6)");
+    
+    for(let i = 0; i < friday.length; i++){
+      friday[i].classList.add("friday")
+    }
+  }
+  fridayDay();  
