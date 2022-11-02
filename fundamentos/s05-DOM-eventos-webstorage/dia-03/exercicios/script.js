ab296856600051553b7255f9068b85aa98dc75f1
Andre Miranda <andy.lm0123@gmail.com>
2022-11-01 23:12:24 -0300

    finalizando partes calendario

--- fundamentos/s05-DOM-eventos-webstorage/dia-03/exercicios/script.js
+++ fundamentos/s05-DOM-eventos-webstorage/dia-03/exercicios/script.js
@@ -23,7 +23,7 @@ const createDays = () => {
     const monthDays = document.getElementById("days");
     let days = document.createElement("li");
     days.innerHTML = decemberDaysList[i];
-    days.classList.add("day")
+    days.classList.add("day");
 
     //CLASS HOLIDAY
     if (decemberDaysList[i] == "24" || decemberDaysList[i] == "25" || decemberDaysList[i] == "31") {
@@ -112,10 +112,92 @@ changeFridayText();
 const sizeHover = () => {
   let days = document.getElementById("days");
   days.addEventListener('mouseover', (event) => { 
-    event.target.style.fontSize= "25px";
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600'; 
   });
   days.addEventListener('mouseout', (event) => {
-    event.target.style.fontSize= "20px";
+    event.target.style.fontSize = '20px';
+    event.target.style.fontWeight = '200';
   })
 };
 sizeHover();
+
+const handleNewTask = (task) => {
+let tasksContainer = document.querySelector('.my-tasks'); 
+let newTask = document.createElement("span");
+
+newTask.innerHTML = task + " ";
+tasksContainer.appendChild(newTask);
+};
+handleNewTask("Cozinhar");
+
+const colorTask = (color) => {
+  let tasksContainer = document.querySelector('.my-tasks'); 
+  let newColor = document.createElement("div");
+
+  newColor.classList.add("task");
+  newColor.style.backgroundColor = color;
+  tasksContainer.appendChild(newColor);
+}
+colorTask("tomato");
+
+const selectTask = () => {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let myTasks = document.querySelector('.task');
+
+  myTasks.addEventListener("click",(event)=>{
+    if (selectedTask.length === 0) {
+      event.target.className = 'task selected';
+    } else {
+      event.target.className = 'task';
+    }
+  })
+};
+selectTask();
+
+const setDayColor = () => {
+  let selectedTask = document.getElementsByClassName('task selected'); // Retorna um array com todos os elementos com a classe "task selected"
+  let days = document.querySelector('#days'); // Seleciona a primeira ul com id "days"
+  let taskDiv = document.querySelector('.task'); // Seleciona o primeiro elemento com a classe "task"
+  let taskColor = taskDiv.style.backgroundColor; // Salva o Background Color da classe task na variável "taskColor"
+  days.addEventListener("click",(event) =>{
+    let eventTargetColor = event.target.style.color;
+    if(selectedTask.length > 0 && eventTargetColor !== taskColor){
+      let color = selectedTask[0].style.backgroundColor; // Pega a cor de fundo do primeiro elemento salvo na variável "selectedTask" e salva na variável "color"
+      event.target.style.color = color; // atribui a cor salva na variável "color" ao evento alvo
+    }else if (eventTargetColor === taskColor) {
+      event.target.style.color = 'rgb(119,119,119)';  // Altera a cor de fundo do evento alvo para "rgb(119, 119, 119)"
+    }
+  })
+}
+setDayColor();
+
+const addNewTask = () => {
+  let getInputField = document.querySelector('#task-input');
+  let addInputButton = document.querySelector('#btn-add');
+  let getTaskList = document.querySelector('.task-list');
+
+  addInputButton.addEventListener('click', () => {
+    if (getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    } else {
+      alert('Error: Digite ao menos 1 caractere.');
+    }
+  });
+
+  getInputField.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter' && getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    }
+  });
+}
+
+addNewTask();
