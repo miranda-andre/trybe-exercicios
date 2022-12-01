bf4a751e77672123c47779c30877ab2863bf53ad
Andre Miranda <andy.lm0123@gmail.com>
2022-11-30 23:12:35 -0300

    exercicio e fixacao 08-2

---
+++ fundamentos/s08-Higher-Order-Functions/dia-02/fixacao/sort.js
@@ -0,0 +1,14 @@
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+];
+
+// Adicione seu código aqui
+people.sort((personA, personB) => personA.age - personB.age);
+console.log(people);
+
+people.sort((personA, personB) => personB.age - personA.age);
+console.log(people);
