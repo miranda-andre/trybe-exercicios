5a1790f518c217ecc75b923fc4adf787410aadea
Andre Miranda <andy.lm0123@gmail.com>
2023-01-23 20:06:49 -0300

    fixacao react feito

---
+++ front-end/s02-intro-react/dia-01/fixacao/src/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+class About extends React.Component {
+    render() {
+        return (
+            <section>
+                <section>
+                    <h1>Andre Miranda</h1>
+                    <p> Eu programo</p>
+                </section>
+                <section>
+                    <h2>Minhas Habilidades</h2>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JS</li>
+                    </ul>
+                </section>
+            </section>
+        )
+    };
+};
+
+export default About;
